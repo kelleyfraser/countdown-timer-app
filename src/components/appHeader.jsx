1923c2976f2a749c54480e858f26cdc67a30e81c
Kelley Fraser <kelleyirwinfraser@gmail.com>
2022-10-18 15:56:18 +0100

    Build and style appHeader shell.

---
+++ src/components/appHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AppHeader = () => {
+  return (
+    <div className="header-container container-fluid">
+      <i className="fa fa-refresh btn-icon fa-lg"></i>
+      <h1 className="appName">countdown timers</h1>
+    </div>
+  );
+}
+
+export default AppHeader;
