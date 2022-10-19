4611a32e89099faddfa0f70a6ebadc06c0f48bff
Kelley Fraser <kelleyirwinfraser@gmail.com>
2022-10-19 14:11:33 +0100

    Change app title.

--- src/components/appHeader.jsx
+++ src/components/appHeader.jsx
@@ -4,7 +4,7 @@ const AppHeader = () => {
   return (
     <div className="header-container container-fluid">
       <i className="fa fa-refresh btn-icon fa-lg"></i>
-      <h1 className="appName">countdown timers</h1>
+      <h1 className="appName">countdown</h1>
     </div>
   );
 }
